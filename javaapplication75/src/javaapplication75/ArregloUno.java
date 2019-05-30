69d232967e3ea3291bb9d779262e57d23289b3fd
dani117m <marcelodaniel-117@hotmail.com>
2019-05-30 09:44:54 -0500

    Arreglos unidemencionales

--- javaapplication75/src/javaapplication75/ArregloUno.java
+++ javaapplication75/src/javaapplication75/ArregloUno.java
@@ -16,10 +16,16 @@ public class ArregloUno {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+        int[] c = new int[5];
+        c[3]=20; 
+        c[1+1]=5+6; 
+        c[2]=11; 
+        c[4]=10;
+        //c[5]=100; 
         
-        int[] c = new int[12];
         
-        
+    System.out.printf("%d + %d = %d \n",c[1],c[4],c[1]+ c[4]);    
     }
     
 }
